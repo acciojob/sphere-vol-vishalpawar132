4b2ba672dde1f29a6194e059748986559222519b
vishal Pawar <133097620+vishalpawar132@users.noreply.github.com>
2023-07-02 11:25:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,22 +1,14 @@
 function volume_sphere() {
-  // Retrieve the input value from the Radius field
+
   var radius = document.getElementById("radius").value;
-
-  // Calculate the volume of the sphere
+	radius = Math.abs(radius);
   var volume = (4 / 3) * Math.PI * Math.pow(radius, 3);
-
-  // Display the calculated volume in the Volume field
-  document.getElementById("volume").value = volume.toFixed(2);
-
-  // Prevent form submission
+  document.getElementById("volume").value = volume.toFixed(4);
   return false;
 }
 
 window.onload = function() {
-  // Get the form element
   var form = document.getElementById('MyForm');
-
-  // Assign the volume_sphere function to the form's onsubmit event
   form.onsubmit = volume_sphere;
 };
 
